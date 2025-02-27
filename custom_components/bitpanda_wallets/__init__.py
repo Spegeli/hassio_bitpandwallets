84d7e3f34cb74973619244e693b1a86e6257bf6d
Spegeli <19636318+Spegeli@users.noreply.github.com>
2025-02-27 19:11:11 +0100

    Update __init__.py

--- custom_components/bitpanda_wallets/__init__.py
+++ custom_components/bitpanda_wallets/__init__.py
@@ -1,20 +1,17 @@
-from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.typing import ConfigType
+from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
-async def async_setup(hass: HomeAssistant, config: ConfigType):
-    """Set up the Bitpanda Wallets integration."""
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Bitpanda from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    return True
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up Bitpanda Wallets from a config entry."""
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    if unload_ok and DOMAIN in hass.data:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
     return unload_ok
