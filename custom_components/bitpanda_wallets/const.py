e7b433378883c5870be86ec1715899754227aae7
Spegeli <19636318+Spegeli@users.noreply.github.com>
2025-02-27 19:11:32 +0100

    Update const.py

--- custom_components/bitpanda_wallets/const.py
+++ custom_components/bitpanda_wallets/const.py
@@ -4,9 +4,19 @@ CONF_WALLET = "wallet"
 CONF_CURRENCY = "currency"
 
 BITPANDA_API_URL = "https://api.bitpanda.com/v1"
+UPDATE_INTERVAL = 5
+
 DEFAULT_FIAT_CURRENCY = "EUR"
 FIAT_CURRENCIES = ["EUR", "USD", "CHF", "GBP", "TRY", "PLN", "HUF", "CZK", "SEK", "DKK"]
+
 WALLET_TYPES = {
-    "FIAT": "Fiat Wallet",
-    "ASSETS": "Asset Wallets"
+    "FIAT": "Fiat",
+    "CRYPTOCOIN": "Crypto",
+    "LEVERAGE": "Leverage",
+    "INDEX": "Crypto Indices",
+#    "SECURITYTOKEN": "Security token",    
+    "STOCK": "Stocks",
+    "ETF": "ETFs",
+    "ETC": "Commodities", 
+    "METAL": "Metals"
 }
